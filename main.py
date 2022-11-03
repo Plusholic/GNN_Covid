2af173dd96a23284b946988cd0a5b406a3db1928
Plusholic <97847752+Plusholic@users.noreply.github.com>
2022-11-04 02:24:36 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,209 +1,188 @@
-from operator import index
-from posixpath import supports_unicode_filenames
-import dgl # huggingface 같은 라이브러리
-import random
-import torch
-import numpy as np
+# import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from processing.data_scaler import *
-from processing.load_data import *
-from trainers.STGCN_trainer import STGCNTrainer
-from utils.utils import *
-from models.model import *
-import torch.nn as nn
-from processing.sensors2graph import *
-import scipy.sparse as sp
-
+from torch.utils.data import DataLoader
 import os
-print(os.getcwd())
+import torch
+import shutil
+
+from lib import PairDataset
+from lib import Data2Graph
+from lib import compute_metrics
+from lib import matplotlib_plot_font
+from lib import save_figure_predict
+from lib import Trainer
+from lib import preprocess_data
+from model_select import model_selection
 
 matplotlib_plot_font()
-torch.manual_seed(42)
-# device = torch.device("mps")
-device = torch.device("cpu")
 
-#######################################
-## Load Data and Train, Test Setting ##
-#######################################
-region_type = 'state'
-df2 = pd.read_csv(f'/Users/jeonjunhwi/문서/Projects/Master_GNN/Data/KCDC_data/Processing_Results/smoothing_3_{region_type}_mean.csv', index_col=0, encoding='cp949')
-# df = df.iloc[100:660]
-# df2 = df
+MODEL_NAME = "GCN"
+diff_ = '1st'
+tmp = '_'+diff_ # custom folder name
+TIME_STEPS = 5
+BATCH_SIZE = 16
+EPOCHS = 50
+learning_rate = 1e-3
+validation_ratio = 0.2
+device = torch.device('cuda', 0) if torch.cuda.is_available() else torch.device('cpu')
 
-df = df2.iloc[100:660] # 12월 까지만 해보자
-df2 = df2.iloc[100:660] # 아래쪽 plot을 위해서 저장해준 원래부분, 깔끔하게 바꾸자
-df = df.diff()
-df = df.iloc[1:, :]
-# print(df)
-# input(" ")
-### 컬럼을 숫자로 바꿔줌 ###
+# Load Data
+region_type = 'city'
+dataset_name = 'covid_alpha'
+dist_mx = pd.read_csv(f'./Data/distances_kr_{region_type}_adj_mx.csv', encoding='cp949', index_col=0)
+
+# data는 1로 고정
+df = pd.read_csv(f'Data/KCDC_data/Processing_Results/smoothing_1_{region_type}_mean.csv', index_col=0, encoding='cp949')
+df = df.iloc[1:707] # 델타 : 554, 540, 533 오미크론 : 707, 693, 686 / 402 ~ 20210226 백신
+# df = df.iloc[:340] # 델타 : 554, 540, 533 오미크론 : 707, 693, 686 / 402 ~ 20210226 백신 ~402
 region_dict = {}
 for i, region in enumerate(df.columns):
-    region_dict[i] = region
+    region_dict[i] = region 
 
-num_samples, num_nodes = df.shape
-print("number of nodes:",num_nodes)
-print("number of samples:",num_samples)
-
-# 시계열 데이터이기 때문에 랜덤 샘플링 하지 않음 훈련 데이터 세트는 과거, 검증 데이터 세트는 항상 약간 뒤의 미래
-# W = adj_mx
-len_val = 25
-len_test = 24
-len_train = df.shape[0] - len_val - len_test
-
-train = df[: len_train]
-val = df[len_train: len_train + len_val]
-test = df[len_train + len_val:len_train + len_val + len_test]
-# print(len(train), len(val), len(test))
-# print('train date : ~', train.index[-1])
-# print('val date : ~', val.index[-1])
-# print('test date : ~', test.index[-1])
-# print(" ")
-train = df.iloc[: len_train,:]
-val = df.iloc[len_train: len_train + len_val,:]
-test = df.iloc[len_train + len_val:len_train + len_val + len_test,:]
-# print(len(train), len(val), len(test))
-# print('train date : ~', train.index[-1])
-# print('val date : ~', val.index[-1])
-# print('test date : ~', test.index[-1])
-# input("ctrl + c")
+#####################
+## TEST START DATE ##
+#####################    
+split_date = '2021-11-25' #'2021-11-25' #'2021-06-25' 
+# split_date = '2020-11-23' # df[:402]
 
 
-############################
-## Hyperparameter Setting ##
-############################
-n_his = 5
-save_path = 'stgcnwavemodel.pt'
-control_str = 'TSNT'#TNTST' #'TSNT'
-num_layers = len(control_str)
-n_pred = 1
-n_route = num_nodes
-blocks = [1, 64, 128, 128, 32, 128]
-# blocks = [1, 32, 128, 64, 32, 128]
-drop_prob = 0
-batch_size = 16
-epochs = 50
-lr = 0.001
+train, val, test, scaler, horizon = preprocess_data(data = df,
+                                                    val_ratio=validation_ratio,
+                                                    split_date=split_date,
+                                                    time_steps=TIME_STEPS,
+                                                    diff_=diff_)
 
-#########################
-## Distance Network 01 ##
-# #######################
-# norm = 0.9
-# graph_type = f'dist_01_{region_type}_{train.index[0]}~{test.index[-1]}'
-# dist_mx = pd.read_csv(f'/Users/jeonjunhwi/문서/Projects/Master_GNN/stgcn_wave/data/sensor_graph/distances_kr_{region_type}_adj_mx.csv', encoding='cp949', index_col=0)
+train_dl = DataLoader(PairDataset(train[0], train[1]), batch_size=BATCH_SIZE, shuffle=True)
+val_dl = DataLoader(PairDataset(val[0], val[1]), batch_size=BATCH_SIZE, shuffle=False)
+test_dl = DataLoader(PairDataset(test[0], test[1]), batch_size=1, shuffle=False)
+# [Batch, node, time, channel]
+
+MAE_total_list, MAPE_total_list, RMSE_total_list, idx_list = [], [], [], []
+
+network = 'dist_01'
+norm = 0.5
+
+
+result_dir = ('%s_%s_%s'+tmp) % (MODEL_NAME, network, norm)
+model_dir = ('%s_%s_%s'+tmp) % (MODEL_NAME, network, norm)
+Network_path = os.path.join('Result', dataset_name, 'Network', str(horizon), result_dir)
+Figure_path = os.path.join('Result', dataset_name, 'Figure', str(horizon), result_dir)
+Diameter_path = os.path.join('Result', dataset_name, 'Diameter', str(horizon), result_dir)
+Pred_path = os.path.join('Result', dataset_name, 'Pred', str(horizon), result_dir)
+model_path = os.path.join('Save_model', dataset_name, str(horizon), model_dir)
+
+if os.path.exists(Network_path):
+    shutil.rmtree(Network_path) # 최종 경로로 해야함
+if os.path.exists(Figure_path):
+    shutil.rmtree(Figure_path) # 해당 경로 데이터 모두 삭제
+if os.path.exists(Diameter_path):
+    shutil.rmtree(Diameter_path) # 해당 경로 데이터 모두 삭제
+if os.path.exists(Pred_path):
+    shutil.rmtree(Pred_path) # 해당 경로 데이터 모두 삭제
+if os.path.exists(model_path):
+    shutil.rmtree(model_path) # 해당 경로 데이터 모두 삭제
     
-# adj_mx = get_adjacency_matrix(dist_mx, normalized_k=norm) #Generates the adjacent matrix from the distance between sensors and the sensor ids. 
-# sp_mx = sp.coo_matrix(adj_mx)
-# G = dgl.from_scipy(sp_mx)
-# pd.DataFrame({'region' : dist_mx.columns, 'degree' : G.in_degrees()}).to_csv('Result/summary/degree.csv', encoding='cp949')
+os.makedirs(Network_path) # 새로 폴더 생성
+os.makedirs(Figure_path) # 새로 폴더 생성
+os.makedirs(Diameter_path) # 새로 폴더 생성
+os.makedirs(Pred_path) # 새로 폴더 생성
+os.makedirs(model_path) # 새로 폴더 생성
 
-#########################
-## Distance Network 02 ##
-# #######################
-# graph_type = f'dist_02_{region_type}_{train.index[0]}~{test.index[-1]}'
-# # distance_df = pd.read_csv('/Users/jeonjunhwi/문서/Projects/Master_GNN//stgcn_wave/data/sensor_graph/distances_kr_metro_city.csv', dtype={'from': 'str', 'to': 'str'}, index_col=0)
-# dist_mx = pd.read_csv(f'/Users/jeonjunhwi/문서/Projects/Master_GNN/stgcn_wave/data/sensor_graph/distances_kr_{region_type}_adj_mx.csv', encoding='cp949', index_col=0)
-# norm = 1
-# G, adj_mx = make_dist_network(dist_mx, threshold=norm)
-# sp_mx = sp.coo_matrix(adj_mx)
-# G = dgl.from_scipy(sp_mx)
+data2network = Data2Graph(distance_matrix = dist_mx, temporal_data = df)
+G, adj_mx, graph_type = data2network.make_network(network_type=network,
+                                                region_type=region_type,
+                                                norm=norm,
+                                                int_adj=False,
+                                                Diameter_path = Diameter_path)
+data2network.save_graph_html(enc=region_dict,
+                             title=region_type,
+                             save_name=f'{graph_type}_{norm}',
+                             Network_path = Network_path)
 
-#########################
-## Correlation Network ##
-#########################
-norm = 0.9
-graph_type = f'test_minus_{region_type}_{train.index[0]}~{test.index[-1]}'
-# daily_df_state = pd.read_csv('/Users/jeonjunhwi/문서/Projects/Master_GNN/stgcn_wave/data/smoothing_3_state_mean.csv', encoding="euc-kr",index_col =0)
-G, adj_mx = make_corr_network(df, threshold=norm, minus_mean=True)
-G = dgl.from_networkx(G)
+######################
+# Save Graph Diameter#
+######################
+import networkx as nx
+G1 = G.to_networkx()
+pd.DataFrame({'degree' : nx.degree_centrality(G1).values(),
+            'eigenvector' : nx.eigenvector_centrality(nx.Graph(G1), max_iter=1000).values(),
+            'closeness' : nx.closeness_centrality(nx.Graph(G1)).values(),
+            'betweness' : nx.betweenness_centrality(nx.Graph(G1)).values(),
+            'clustering_coeff' : nx.clustering(nx.Graph(G1)).values()},
+            index= dist_mx.index
+            ).to_csv(f"{Diameter_path}/{MODEL_NAME}_{graph_type}_{norm}_diameter.csv",encoding='cp949')        
+print('Confirm Symmetric(False is Symmetric) : ', False in (adj_mx.numpy() == adj_mx.numpy().transpose()))
 
-# ####################################
-# ### node degree, graph html 저장 ###
-# ####################################
-pd.DataFrame({'region' : df.columns, 'degree' : G.in_degrees()}).to_csv(f'Result/summary/{graph_type}_degree.csv', encoding='cp949')
-save_graph_html(adj_mx = adj_mx,
-                enc = region_dict,
-                title = graph_type + ' ' + str(norm),
-                save_name = f'Result/html/state_network_vis_{graph_type}_{norm}')
-# input("Press Enter after See Network File ")
+###############
+# Train Model #
+###############
+model = model_selection(MODEL_NAME = MODEL_NAME, adj_mx = adj_mx, TIME_STEPS = TIME_STEPS, device=device, save_path = model_path)
 
-##########################################
-## Data Scaling and Generate Dataloader ##
-##########################################
+loss_func = torch.nn.MSELoss()
+optimizer = torch.optim.NAdam(model.parameters(), lr=learning_rate)
 
-scaler = StandardScaler()
-# scaler = MinMaxScaler()
-# scaler = FluctuationScaler(df)
-# scaler = LogScaler(log_category='log10')
-suptitle_ = f"Standard_{graph_type}_{norm}_{control_str}_NAdam_GCN2"
-train = scaler.fit_transform(train)
-val = scaler.transform(val)
-test = scaler.transform(test)
+trainer = Trainer(model=model,
+                train_loader=train_dl,
+                val_loader=val_dl,
+                test_loader=test[0],
+                adj=adj_mx,
+                scaler=scaler,
+                loss=loss_func,
+                optimizer=optimizer,
+                device=device,
+                save_path=model_path,
+                callbacks=None,
+                raw_test=df.iloc[-(horizon + 1):].values)
+trainer.train(EPOCHS)
+y_pred= trainer.predict()
 
-# tensor data로 변환
-x_train, y_train = data_transform(train, n_his, n_pred, device)
-x_val, y_val = data_transform(val, n_his, n_pred, device)
-x_test, y_test = data_transform(test, n_his, n_pred, device)
-print(x_train.shape, y_train.shape)
-print(x_val.shape, y_val.shape)
-print(x_test.shape, y_test.shape)
-input(" ctrl + c : ")
-# DataLoader 생성
-train_data = torch.utils.data.TensorDataset(x_train, y_train)
-train_loader = torch.utils.data.DataLoader(train_data, batch_size, shuffle=True)
-val_data = torch.utils.data.TensorDataset(x_val, y_val)
-val_loader = torch.utils.data.DataLoader(val_data, batch_size)
-test_data = torch.utils.data.TensorDataset(x_test, y_test)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=1)
-# for x, y in train_loader:
-#     print(x.shape, y.shape)
+###################
+# Save Prediction #
+###################
+df.iloc[-horizon:].to_csv(f'{Pred_path}/ground_truth.csv', encoding='cp949')
+df2 = pd.DataFrame(
+                   y_pred,
+                   columns=df.columns,
+                   index=df.iloc[-horizon:].index
+                   ).to_csv(f'{Pred_path}/{graph_type}_{norm}.csv', encoding='cp949')
 
-# print(train_loader)
-# print(val_loader)
-# print(test_loader)
-loss = nn.MSELoss()
-G = G.to(device)
+################################
+# Compute RMSE of test dataset #
+################################
+RMSE, RMSE_total = compute_metrics(df.iloc[-horizon:], y_pred, metric='rmse')
+MAE, MAE_total = compute_metrics(df.iloc[-horizon:], y_pred, metric='mae')
+MAPE, MAPE_total = compute_metrics(df.iloc[-horizon:], y_pred, metric='mape')
 
-model = STGCN_WAVE(blocks, n_his, n_route, G, drop_prob, num_layers, device, control_str).to(device)
+###############
+# save figure #
+###############
+suptitle = f"{MODEL_NAME}_{graph_type}_{norm}"
+save_figure_predict(df, y_pred,
+                    None, None, len(test[0]),
+                    n_his_pred = TIME_STEPS,
+                    region_dict = region_dict,
+                    suptitle = suptitle,
+                    date_split = f"{df.index[1]} ~ {df.index[len(train[0])]} ~ {df.index[len(train[0])+TIME_STEPS + len(val[0])+TIME_STEPS*2]} ~ {df.index[-1]}",
+                    MAE = MAE, MAPE = MAPE, RMSE = RMSE,
+                    MAE_total = MAE_total, MAPE_total = MAPE_total, RMSE_total = RMSE_total, 
+                    PATH=Figure_path)
 
+MAE_total_list.append(MAE_total)
+MAPE_total_list.append(MAPE_total)
+RMSE_total_list.append(RMSE_total)
+idx_list.append(f'{network}_{norm}')
 
-optimizer = torch.optim.NAdam(model.parameters(), lr=lr)
-# optimizer = torch.optim.RMSprop(model.parameters(), lr=lr)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.7)
+########################################
+# Save for Total Metric of Each Metric #
+########################################
+pd.DataFrame({
+            'MAE' : MAE,
+            'MAPE' : MAPE,
+            'RMSE' : RMSE,
+            }, index=region_dict.values()).to_csv(f'{Pred_path}/{suptitle}_metric.csv', encoding='cp949')
 
-
-trainer = STGCNTrainer(model=model,
-                       train_loader=train_loader,
-                       val_loader=val_loader,
-                       test_loader=test_loader,
-                       loss=loss,
-                       optimizer=optimizer,
-                       scaler=scaler,
-                       device=device,
-                       save_path=save_path,
-                       scheduler=scheduler,
-                       raw_test=df2.iloc[-(len(y_test) + 1):].values)
-
-trainer.train(epochs)
-# sequence_length = 5
-
-gru_res = pd.DataFrame({})
-lstm_res = pd.DataFrame({})
-
-(MAE, MAPE, RMSE), (MAE_total, MAPE_total, RMSE_total), y_pred = trainer.predict(df.shape[1])
-
-gru_res, lstm_res = None, None
-
-
-pd.DataFrame(y_pred.numpy(), columns=[i for i in range(df2.shape[1])]).to_csv(f"data/{graph_type}_{norm}.csv")
-save_figure_predict(df2, y_pred,
-                    gru_res, lstm_res,
-                    len_train, len_val, len_test, n_his,
-                    region_dict, suptitle_,
-                    MAE, MAPE, RMSE,
-                    MAE_total, MAPE_total, RMSE_total, 
-                    'Result/')
-
-print(f'MAE : {MAE_total}, MAPE : {MAPE_total}, RMSE : {RMSE_total}')
-
+pd.DataFrame({
+            'MAE' : MAE_total_list,
+            'MAPE' : MAPE_total_list,
+            'RMSE' : RMSE_total_list
+              }, index=idx_list).to_csv(f'{Pred_path}/{suptitle}_total_metric.csv', encoding='cp949')
---
+++ model_select.py
@@ -0,0 +1,172 @@
+import pickle
+
+
+def model_selection(MODEL_NAME = None,
+                    adj_mx = None,
+                    TIME_STEPS = None,
+                    device=None,
+                    save_path = None):
+
+    ###### ASTGCN MODEL ######
+    if MODEL_NAME == 'ASTGCN':
+        from models import make_model
+        print(MODEL_NAME)
+        # nb_block = 1 # block 갯수 : 이거로 hop을 생각할 수 있을듯. layer의 개념이니까.
+        # K = 3 # 확인해봐야함
+
+        config = dict({'num_of_vertices' : adj_mx.shape[0],
+                    #  'points_per_hour' : 12,
+                     'num_for_predict' : 1,
+                     'in_channels' : 1,
+                     'nb_block' : 1,
+                     'K' : 3,
+                     'nb_chev_filter' : 16,
+                     'nb_time_filter' : 16,
+                     'time_strides' : 1})
+        
+        model = make_model(DEVICE=device,
+                           nb_block=config['nb_block'],
+                           in_channels=config['in_channels'],
+                           K = config['K'],
+                           nb_chev_filter=config['nb_chev_filter'],
+                           nb_time_filter=config['nb_time_filter'],
+                           time_strides=config['time_strides'],
+                           adj_mx=adj_mx.numpy(),
+                           num_for_predict=config['num_for_predict'],
+                           len_input=TIME_STEPS,
+                           num_of_vertices=config['num_of_vertices'])
+    
+    ###### TGCN MODEL ######    
+    if MODEL_NAME == 'TGCN':
+        print(MODEL_NAME)
+        from models import TGCNConv
+        
+        config = dict({'hidden_dim' : 64,
+                     'out_dim' : 64,
+                     'num_hop' : 2})
+        
+        model = TGCNConv(adj_mx = adj_mx,
+                        hidden_dim=config['hidden_dim'],
+                        out_dim=config['out_dim'],
+                        num_hop=config['num_hop'])
+    
+    ###### STGNN MODEL ######
+    if MODEL_NAME == 'STGNN':
+        print(MODEL_NAME)
+        from models import ProposedSTGNN
+        
+        config = dict({'predicted_time_steps' : 1,
+                     'in_channels' : 1,
+                     'spatial_channels' : 16,
+                     'spatial_hidden_channels' : 16,
+                     'spatial_out_channels' : 16,
+                     'out_channels' : 16,
+                     'temporal_kernel' : 3,
+                     'num_hop' : 2,
+                     'drop_rate' : 0.2})
+        
+        model = ProposedSTGNN(n_nodes=adj_mx.shape[0],
+                              adj_mx=adj_mx,
+                              time_steps=TIME_STEPS,
+                              predicted_time_steps=config['predicted_time_steps'],
+                              in_channels=config['in_channels'],
+                              spatial_channels=config['spatial_channels'],
+                              spatial_hidden_channels=config['spatial_hidden_channels'], # 16 # 오미크론 64-64 델타 16-16
+                              spatial_out_channels=config['spatial_out_channels'], # 16
+                              out_channels=config['out_channels'], # 16
+                              temporal_kernel=config['temporal_kernel'],
+                              num_hop=config['num_hop'],
+                              drop_rate=config['drop_rate']).to(device=device)
+
+    ###### GCN MODEL ######
+    if MODEL_NAME == 'GCN':
+        from models import GCN
+        print(MODEL_NAME)
+        config = dict({'hidden_feats' : [16],#, 64],
+                     'activation' : None,
+                     'residual' : None,
+                     'batchnorm' : None,
+                     'dropout' : [0.5],
+                     'gnn_norm' : None})
+        
+        model = GCN(in_feats=1,
+                    hidden_feats=config['hidden_feats'],
+                    adj_mx = adj_mx,
+                    TIME_STEPS=TIME_STEPS,
+                    activation=config['activation'],
+                    residual=config['residual'],
+                    batchnorm=config['batchnorm'],
+                    dropout=config['dropout'],
+                    gnn_norm=config['gnn_norm'],
+                    device=device).to(device=device)
+        
+    ###### DCRNN MODEL ######
+    if MODEL_NAME == 'DCRNN':
+        from models import DCRNNModel
+        config = dict({'rnn_units' : 64,
+                     'num_rnn_layers' : 2,
+                     'horizon' : 1,
+                     'max_diffusion_step' : 2,
+                     'cl_decay_steps' : 2000,
+                     'filter_type' : 'dual_random_walk',
+                     'use_curriculum_learning' : False})
+        
+        model = DCRNNModel(adj_mx=adj_mx,
+                        input_dim=1,
+                        output_dim=1, # 확인필요
+                        rnn_units=config['rnn_units'],
+                        num_rnn_layers=config['num_rnn_layers'],
+                        seq_len=TIME_STEPS,
+                        horizon=config['horizon'],
+                        max_diffusion_step=config['max_diffusion_step'],
+                        cl_decay_steps=config['cl_decay_steps'],
+                        filter_type=config['filter_type'],
+                        num_node=adj_mx.shape[0],
+                        use_curriculum_learning=config['use_curriculum_learning']).to(device=device)
+
+    ###### GCN2 MODEL ######
+    if MODEL_NAME == 'GCN2':
+        print(MODEL_NAME)
+        from models import GCNII
+        config = dict({'nlayers' : 1,
+                     'nhidden' : 64,
+                     'dropout' : 0.2,
+                     'lambda' : 0.5,
+                     'alpha' : 0.1,
+                     'nclass' : 1,
+                     'variant':False})
+        model = GCNII(adj_mx = adj_mx,
+                    TIME_STEPS = TIME_STEPS,
+                    nfeat=1,
+                    nlayers=config['nlayers'],
+                    nhidden=config['nhidden'],
+                    nclass=config['nclass'], #확인필요
+                    dropout=config['dropout'],
+                    lamda=config['lambda'],
+                    alpha=config['alpha'],
+                    variant=config['variant']).to(device=device)
+
+    ###### STGCN MODEL ######
+    if MODEL_NAME == 'STGCN':
+        print(MODEL_NAME)
+        from models import STGCN_WAVE
+        import networkx as nx
+        import dgl
+        
+        config = dict({'control_str' : 'TSNT', # T로 끝나면 ([16, 1, 1, 229])
+                       'blocks' : [1, 64, 64, 64, 32, 128],
+                       'drop_prob' : 0})
+        
+        G = dgl.from_networkx(nx.Graph(adj_mx.numpy()))
+        model = STGCN_WAVE(c=config['blocks'],
+                           T=TIME_STEPS,
+                           n=adj_mx.shape[0], #num_node
+                           Lk=G,
+                           p=config['drop_prob'], # 이거 작동안함
+                           num_layers=len(config['control_str']),
+                           device=device,
+                           control_str=config['control_str']).to(device)
+
+    with open(f'{save_path}/config.pkl', 'wb') as f:
+        pickle.dump(config, f)
+    return model
